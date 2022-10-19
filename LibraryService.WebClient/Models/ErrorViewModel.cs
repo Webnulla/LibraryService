4aa4587de3b31c66e2861ef051a8735c21040784
Konstantin <muselsps@ya.ru>
2022-10-19 15:45:40 +0300

    Initial commit

---
+++ LibraryService.WebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LibraryService.WebClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ LibraryService/LibraryWebService.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using LibraryService.Models;
+using LibraryService.Services;
+using LibraryService.Services.Impl;
+
+namespace LibraryService
+{
+    /// <summary>
+    /// Summary description for LibraryWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class LibraryWebService : System.Web.Services.WebService
+    {
+        private readonly ILibraryRepositoryService _libraryRepositoryService;
+        
+        public LibraryWebService()
+        {
+            _libraryRepositoryService = new LibraryRepository(new LibraryDatabaseContext());
+        }
+        
+        [WebMethod]
+        public Book[] GetBookByTitle(string title)
+        {
+            return _libraryRepositoryService.GetByTitle(title).ToArray();
+        }
+        
+        [WebMethod]
+        public Book[] GetBookByCategory(string category)
+        {
+            return _libraryRepositoryService.GetByCategory(category).ToArray();
+        }
+        
+        [WebMethod]
+        public Book[] GetBookByAuthor(string authorName)
+        {
+            return _libraryRepositoryService.GetByAuthor(authorName).ToArray();
+        }
+    }
+}
